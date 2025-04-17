9e4462db7f2f81dd3e2c3080a41ddbfbd391627a
mlamsey <31256490+mlamsey@users.noreply.github.com>
2025-04-17 09:49:22 -0700

    checkin robot params for ROS stuff

--- body/stretch_body/robot_params_RE2V0.py
+++ body/stretch_body/robot_params_RE2V0.py
@@ -127,8 +127,7 @@ RE2V0_stretch_gripper={
         'enable_runstop': 1,
         'disable_torque_on_stop': 1,
         'range_t': [0, 8022],
-        'zero_t': 5212,
-        'distance_tol': 0.015}
+        'zero_t': 5212}
 
 RE2V0_wrist_yaw={
     'device_info': "Standard wrist yaw shipped with original RE2",
@@ -176,8 +175,7 @@ RE2V0_wrist_yaw={
     'disable_torque_on_stop': 1,
     'range_pad_t': [100.0, -100.0],
     'range_t': [0,9340],
-    'zero_t': 7005,
-    'distance_tol': 0.015}
+    'zero_t': 7005}
 
 #Brought in from tool_share for DexWrist "2"
 RE2V0_wrist_pitch_DW2={
@@ -218,8 +216,7 @@ RE2V0_wrist_pitch_DW2={
         'disable_torque_on_stop': 0,
         'float_on_stop': 1,
         'current_float_A': -0.13,
-        'current_limit_A': 2.5,
-        'distance_tol': 0.03
+        'current_limit_A': 2.5
     }
 
 RE2V0_wrist_roll_DW2={
@@ -260,8 +257,7 @@ RE2V0_wrist_roll_DW2={
         'disable_torque_on_stop': 0,
         'float_on_stop': 0,
         'current_float_A': 0.04,
-        'current_limit_A': 1.0,
-        'distance_tol': 0.015
+        'current_limit_A': 1.0
     }
 
 # ######### EndOfArm Defn ##############
@@ -343,12 +339,14 @@ RE2V0_tool_stretch_dex_wrist={
         'wrist_pitch': {
             'py_class_name': 'WristPitch',
             'py_module_name': 'stretch_body.wrist_pitch',
-            'device_params':'RE2V0_wrist_pitch_DW2'
+            'device_params':'RE2V0_wrist_pitch_DW2',
+            'ros_py_module_name': 'stretch_tool_share.stretch_dex_wrist.command_groups',
         },
         'wrist_roll': {
             'py_class_name': 'WristRoll',
             'py_module_name': 'stretch_body.wrist_roll',
-            'device_params': 'RE2V0_wrist_roll_DW2'
+            'device_params': 'RE2V0_wrist_roll_DW2',
+            'ros_py_module_name': 'stretch_tool_share.stretch_dex_wrist.command_groups',
         },
         'wrist_yaw': {
             'py_class_name': 'WristYaw',
@@ -382,12 +380,16 @@ REV20_tool_stretch_dex_bubble={
         'wrist_pitch': {
             'py_class_name': 'WristPitch',
             'py_module_name': 'stretch_body.wrist_pitch',
-            'device_params':'RE2V0_wrist_pitch_DW2'
+            'device_params':'RE2V0_wrist_pitch_DW2',
+            'ros_py_module_name': 'stretch_tool_share.stretch_dex_bubble.command_groups',
+            'ros_py_class_name': 'WristPitchCommandGroup',
         },
         'wrist_roll': {
             'py_class_name': 'WristRoll',
             'py_module_name': 'stretch_body.wrist_roll',
-            'device_params': 'RE2V0_wrist_roll_DW2'
+            'device_params': 'RE2V0_wrist_roll_DW2',
+            'ros_py_module_name': 'stretch_tool_share.stretch_dex_bubble.command_groups',
+            'ros_py_class_name': 'WristRollCommandGroup',
         },
         'wrist_yaw': {
             'py_class_name': 'WristYaw',
@@ -441,8 +443,7 @@ nominal_params={
                 'vel_m': 0.4,
                 'accel_m': 0.4},
         'vel_brakezone_factor': 0.03},
-        'set_safe_velocity': 1,
-        'distance_tol': 0.008},
+        'set_safe_velocity': 1},
     'base':{
         'usb_name_left_wheel': '/dev/hello-motor-left-wheel',
         'usb_name_right_wheel': '/dev/hello-motor-right-wheel',
@@ -543,8 +544,7 @@ nominal_params={
         'stall_min_vel': 0.1,
         'range_pad_t':[50.0,-50.0],
         'range_t': [0, 3827],
-        'zero_t': 1250,
-        'distance_tol': 0.15},
+        'zero_t': 1250},
     'head_tilt':{
         'flip_encoder_polarity': 1,
         'gr': 1.0,
@@ -592,8 +592,7 @@ nominal_params={
         'stall_min_vel': 0.1,
         'range_pad_t': [50.0, -50.0],
         'range_t': [1775,3150],
-        'zero_t': 2048,
-        'distance_tol': 0.52},
+        'zero_t': 2048},
     'hello-motor-arm':{
         'gains':{
             'effort_LPF': 10.0,
@@ -770,8 +769,7 @@ nominal_params={
               'vel_m': 0.15},
         'vel_brakezone_factor': 0.01},
         'set_safe_velocity': 1,
-        'pinion_t': 12,
-        'distance_tol': 0.015},
+          'pinion_t': 12},
     'pimu':{
       'usb_name': '/dev/hello-pimu',
       'base_fan_off': 70,
